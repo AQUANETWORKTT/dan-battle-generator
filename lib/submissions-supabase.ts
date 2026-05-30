165e662a21ef389a6d707f279582dd17a8d87952
James Dicker <ajcardsstore@gmail.com>
2026-05-30 15:06:28 +0100

    wow

--- lib/submissions-supabase.ts
+++ lib/submissions-supabase.ts
@@ -1,5 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(
+  "SUPABASE URL:",
+  process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL
+);
+
 export const submissionsSupabase = createClient(
   process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY!
