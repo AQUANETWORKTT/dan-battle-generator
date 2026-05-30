df2ee7bc85c67f7c9533c5b7727c7491af7880dd
James Dicker <ajcardsstore@gmail.com>
2026-05-30 15:15:08 +0100

    wow

--- lib/submissions-supabase.ts
+++ lib/submissions-supabase.ts
@@ -1,10 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL || "";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_URL ||
+  "https://dxupgmsscysvztxdtaku.supabase.co";
+
 const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY || "";
+  process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY ||
+  "sb_publishable_C6sf3uuc6zW9_RAtxb7UbA_G4S3G-h_";
 
 export const submissionsSupabase = createClient(
-  supabaseUrl || "https://dxupgmsscysvztxdtaku.supabase.co",
+  supabaseUrl,
   supabaseAnonKey
 );
