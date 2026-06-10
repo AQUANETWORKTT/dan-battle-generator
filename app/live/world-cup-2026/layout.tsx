54641dd487d13f1ef53cfa17d74cf49c3ed9f3df
James Dicker <ajcardsstore@gmail.com>
2026-06-10 16:48:08 +0100

    wow

---
+++ app/live/world-cup-2026/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "World Cup 2026",
+  description: "World Cup 2026 live leaderboard",
+  openGraph: {
+    title: "World Cup 2026",
+    description: "View the live World Cup 2026 rankings.",
+    images: ["/world-cup/logo.png"],
+  },
+};
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
