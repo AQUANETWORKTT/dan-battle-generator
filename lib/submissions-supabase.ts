1faa08f02eb0298388cd2021ea9c1b3643c9d5ed
James Dicker <ajcardsstore@gmail.com>
2026-06-19 15:18:34 +0100

    wow

--- lib/submissions-supabase.ts
+++ lib/submissions-supabase.ts
@@ -6,7 +6,7 @@ const supabaseUrl =
 
 const supabaseAnonKey =
   process.env.NEXT_PUBLIC_SUBMISSIONS_SUPABASE_ANON_KEY ||
-  "sb_publishable_C6sf3uuc6zW9_RAtxb7UbA_G4S3G-h_";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dXBnbXNzY3lzdnp0eGR0YWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODU2MzUsImV4cCI6MjA5MjM2MTYzNX0.Iek_fKX47_95Vxvo3V6C_5neh6OQay-2TGgf_VTBAZE";
 
 export const submissionsSupabase = createClient(
   supabaseUrl,
