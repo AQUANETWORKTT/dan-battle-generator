fcc5f6ea98d1da3f6ad333049a858120f8ff0478
James Dicker <ajcardsstore@gmail.com>
2026-08-09 18:43:21 +0100

    wow

--- supabase/battle_network_realtime.sql
+++ supabase/battle_network_realtime.sql
@@ -12,3 +12,13 @@ begin
     alter publication supabase_realtime add table public.poster_templates;
   end if;
 end $$;
+
+do $$
+begin
+  if not exists (
+    select 1 from pg_publication_tables
+    where pubname = 'supabase_realtime' and schemaname = 'public' and tablename = 'battle_network_battles'
+  ) then
+    alter publication supabase_realtime add table public.battle_network_battles;
+  end if;
+end $$;
